e2996cf8a9b1acd198c82ab7be696e4403f7fdc3
André Martins <fmartins.andre@gmail.com>
2022-04-05 11:24:30 -0300

    chore: add react configs

--- jest.config.ts
+++ jest.config.ts
@@ -5,13 +5,13 @@ const config: Config.InitialOptions = {
   clearMocks: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}'
   ],
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1'
